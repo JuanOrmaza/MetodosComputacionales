2a084e22f2ef9bee2656a68c78cb55ab5ced196c
JuanOrmaza <52267568+JuanOrmaza@users.noreply.github.com>
2019-07-05 12:06:06 -0500

    Update OrmazaJuanAlejandro_S5C3_plots.py

--- S5C3/OrmazaJuanAlejandro_S5C3_plots.py
+++ S5C3/OrmazaJuanAlejandro_S5C3_plots.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 datos=np.genfromtxt('datos.txt')
 plt.figure(figsize=(10,10))
 plt.plot(datos[:,0],datos[:,1])
+plt.title('Leap Frog')
 plt.xlabel('tiempo t[s]')
 plt.ylabel('x(t)')
 plt.savefig('grafica.png')
