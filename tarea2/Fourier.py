d78bf418d963afc43adccadfb6a4c515057e243e
JuanOrmaza <juanalerey@hotmail.com>
2019-07-12 17:25:51 +0000

    se logro completar el punto 2 de la tarea, se sumaron las imagenes y se cumple la funcion

---
+++ tarea2/Fourier.py
@@ -0,0 +1,77 @@
+import numpy as np
+import matplotlib.pylab as plt
+from scipy.fftpack import fft, fftfreq, ifft2, fft2
+
+cara1=plt.imread('cara_02_grisesMF.png')
+cara2=plt.imread('cara_03_grisesMF.png')
+
+trans1=np.fft.fft2(cara1)
+trans2=np.fft.fft2(cara2)
+frec1=np.fft.fftshift(trans1)
+frec2=np.fft.fftshift(trans1)
+
+from matplotlib.colors import LogNorm
+plt.clf()
+plt.figure(figsize=(20,16))
+plt.suptitle("Transformadas de fourier y frecuencias", fontsize=20)
+plt.subplot(2,2,1)
+plt.title("cara 1")
+plt.imshow(abs(trans1),norm=LogNorm())
+plt.colorbar()
+plt.subplot(2,2,2)
+plt.title("cara 2")
+plt.imshow(abs(trans2),norm=LogNorm())
+plt.colorbar()
+plt.subplot(2,2,3)
+plt.title(" frecuencias cara 1")
+plt.imshow(abs(frec1),norm=LogNorm())
+plt.colorbar()
+plt.subplot(2,2,4)
+plt.title("frecuencias cara 2")
+plt.imshow(abs(frec2),norm=LogNorm())
+plt.colorbar()
+
+plt.savefig('transformadasyfrecuencias.png')
+
+def filtroaltas(b,c):
+    tam=np.shape(b)
+    for i in range(tam[0]):
+        for j in range(tam[1]):
+            if abs(b[i,j])<c:
+                b[i,j]=0
+    return b
+
+def filtrobajas(b,c):
+    tam=np.shape(b)
+    for i in range(tam[0]):
+        for j in range(tam[1]):
+            if abs(b[i,j])>c:
+                b[i,j]=0
+    return b
+
+
+filtro1=filtrobajas(trans1,30)
+filtro2=filtroaltas(trans2,70)
+
+plt.clf()
+plt.figure(figsize=(10,8))
+plt.suptitle("filtros", fontsize=20)
+plt.subplot(1,2,1)
+plt.title("cara 1")
+plt.imshow(abs(filtro1),norm=LogNorm())
+plt.colorbar()
+plt.subplot(1,2,2)
+plt.title("cara 2")
+plt.imshow(abs(filtro2),norm=LogNorm())
+plt.colorbar()
+plt.savefig('filtros.png')
+
+imagensuma=np.fft.ifftshift(filtro1)+np.fft.ifftshift(filtro2)
+imagen=ifft2(imagensuma)
+
+plt.clf()
+plt.figure(figsize=(10,10))
+plt.title("imagen sobremontada")
+plt.imshow(abs(imagen),norm=LogNorm(),cmap='gray')
+plt.savefig("resultado.png")
+
