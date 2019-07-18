603afca50b624f6793ee02b8a0efedc7697f61e6
JuanOrmaza <juanalerey@hotmail.com>
2019-07-18 04:02:57 +0000

    se anadio el metodo de euler, leapfrog y rungekutta

--- tarea2/ODEs.cpp
+++ tarea2/ODEs.cpp
@@ -3,7 +3,7 @@
 #include <math.h>
 using namespace std;
 
-float velocidad(vel){
+float velocidad(float vel){
     return vel;
 }
 
@@ -12,6 +12,90 @@ float fuerza(float a, float b){
     float G=6.674*1e-11;
     return -(G*masasol*a)/pow(a*a+b*b,3/2);
 }
+
+
+float Euler(float x0,float y0,float vx0,float vy0,float dt,int num){
+    float xprima1=velocidad(vx0);
+    float xprima2=fuerza(x0,y0);
+    float yprima1=velocidad(vy0);
+    float yprima2=fuerza(y0,x0);
+    int puntos = 20/dt;
+    float t[puntos];
+    
+    float posx[puntos];
+    float velx[puntos];
+    float fuerx[puntos];
+    
+    float posy[puntos];
+    float vely[puntos];
+    float fuery[puntos];
+    
+    
+    t[0]=0.0;
+    posx[0]=x0;
+    posy[0]=y0;
+    velx[0]=xprima1;
+    vely[0]=yprima1;
+    fuerx[0]=xprima2;
+    fuery[0]=yprima2;
+    for(int i=1; i<puntos; i++){
+        t[i] = t[i-1] + dt;
+        posx[i] = posx[i-1] + dt*velocidad(velx[i-1]);
+        velx[i] = velx[i-1] + dt*fuerza(posx[i-1],posy[i-1]);
+        posy[i] = posy[i-1] + dt*velocidad(vely[i-1]);
+        vely[i] = vely[i-1] + dt*fuerza(posy[i-1],posx[i-1]);
+    }
+    ofstream outfile;
+    outfile.open("datoseuler"+to_string(num)+".txt");
+    for(int i=0; i<puntos; i++){
+        outfile<<t[i]<<" "<<posx[i]<<" "<<velx[i]<<" "<<posy[i]<<" "<<vely[i]<<endl;
+    }
+}
+
+float leapfrog(float x0,float y0,float vx0,float vy0,float dt,int num){
+    float xprima1=velocidad(vx0);
+    float xprima2=fuerza(x0,y0);
+    float yprima1=velocidad(vy0);
+    float yprima2=fuerza(y0,x0);
+    int puntos = 20/dt;
+    float t[puntos];
+    
+    float posx[puntos];
+    float velx[puntos];
+    float fuerx[puntos];
+    
+    float posy[puntos];
+    float vely[puntos];
+    float fuery[puntos];
+    
+    
+    t[0]=0.0;
+    t[1]=dt;
+    posx[0]=x0;
+    posy[0]=y0;
+    velx[0]=xprima1;
+    vely[0]=yprima1;
+    posx[1] = posx[0] + dt*velocidad(velx[0]);
+    posy[1] = posy[0] + dt*velocidad(vely[0]);
+    fuerx[0]=xprima2;
+    fuery[0]=yprima2;
+    velx[1] = velx[0] + dt*fuerza(posx[0],posy[0]);
+    vely[1] = vely[0] + dt*fuerza(posy[0],posx[0]);
+    for(int i=2; i<puntos; i++){
+        t[i] = t[i-1] + dt;
+        posx[i] = posx[i-2] + 2*dt*velocidad(velx[i-1]);
+        velx[i] = velx[i-2] + 2*dt*fuerza(posx[i-1],posy[i-1]);
+        posy[i] = posy[i-2] + 2*dt*velocidad(vely[i-1]);
+        vely[i] = vely[i-2] + 2*dt*fuerza(posy[i-1],posx[i-1]);
+    }
+    ofstream outfile;
+    outfile.open("datosleap"+to_string(num)+".txt");
+    for(int i=0; i<puntos; i++){
+        outfile<<t[i]<<" "<<posx[i]<<" "<<velx[i]<<" "<<posy[i]<<" "<<vely[i]<<endl;
+    }
+}
+
+
 
 int main()
 {
@@ -23,44 +107,18 @@ int main()
     float dt1=0.1;
     float dt2=0.01;
     float dt3=0.001;
+    Euler(x0,y0,vx0,vy0,dt1,1);
+    Euler(x0,y0,vx0,vy0,dt2,2);
+    Euler(x0,y0,vx0,vy0,dt3,3);
+    leapfrog(x0,y0,vx0,vy0,dt1,1);
+    leapfrog(x0,y0,vx0,vy0,dt2,2);
+    leapfrog(x0,y0,vx0,vy0,dt3,3);
     
 }
 
 
 
-
-    
-}
-
-/*
-double leapfrog(int tam, double dt)
-{
-    double *v= new double[tam];
-    double *x= new double[tam];
-    double *t= new double[tam];
-    double k;
-    double m;
-    k=300.0;
-    m=2.0;
-    double km;
-    km=k/m;
-    x[0]=0.1;
-    v[0]=(dt/2.0)*(km)*x[0];
-    t[0]=0;
-    for(int i=1;i<tam;i++){
-        v[i]=-(2*dt)*(km)*x[i-1]+v[i-1];
-        x[i]=2*dt*v[i]+x[i-1];
-        t[i]=i*dt;
-    }
-    for(int i=0;i<tam;i++)
-    {
-        cout<<*(t+i)<<" "<<*(x+i)<<" "<<*(v+i)<<endl;
-    }
-    return 0;
-}
-*/
-
-double runge( float x0,float y0,float vx0,float vy0,float dt)
+float runge(float x0,float y0,float vx0,float vy0,float dt)
 {
     //inicio
     float xprima1=velocidad(vx0);
@@ -72,7 +130,9 @@ double runge( float x0,float y0,float vx0,float vy0,float dt)
     float x1 = x0 + dt/2;
     float vx1 = vx0 + 
     
-    /*
+    
+*/        
+         /*
     k_1_prime1 = func_prime_1(x_old,y1_old, y2_old)
     k_1_prime2 = func_prime_2(x_old,y1_old, y2_old)
     
@@ -106,21 +166,25 @@ double runge( float x0,float y0,float vx0,float vy0,float dt)
     y_1_new = y1_old + h * average_k_1
     y_2_new= y2_old + h * average_k_2
     return x_new, y_1_new, y_2_new
-    */
-}
+    
+    
+    
+    
+    
+    
+    x[0]   = min_x
+y_1[0] = 1.0
+y_2[0] = 0.0    
 
-/*
-int main()
-{
-    double h=0.01;
-    double tmax;
-    double tmin;
-    tmax=5;
-    tmin=0;
-    int size;
-    size=(tmax-tmin)/h;
-    leapfrog(size,h);
-    return 0;
-}
-
+for i in range(1,n_points):
+    x[i],y_1[i],y_2[i] = RungeKuttaFourthOrderStep(x[i-1], y_1[i-1], y_2[i-1])
 */
+        
+        
+        
+        
+        
+        
+        
+        
+        
