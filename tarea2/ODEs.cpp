f74375246749ef259876c54173304abe12ba036e
JuanOrmaza <juanalerey@uniandes.edu.co>
2019-07-18 19:21:20 +0000

    se implemento rungekutta

--- tarea2/ODEs.cpp
+++ tarea2/ODEs.cpp
@@ -25,11 +25,9 @@ float Euler(float x0,float y0,float vx0,float vy0,float dt,int num){
     
     float posx[puntos];
     float velx[puntos];
-    float fuerx[puntos];
-    
+     
     float posy[puntos];
     float vely[puntos];
-    float fuery[puntos];
     
     
     t[0]=0.0;
@@ -37,8 +35,6 @@ float Euler(float x0,float y0,float vx0,float vy0,float dt,int num){
     posy[0]=y0;
     velx[0]=xprima1;
     vely[0]=yprima1;
-    fuerx[0]=xprima2;
-    fuery[0]=yprima2;
     for(int i=1; i<puntos; i++){
         t[i] = t[i-1] + dt;
         posx[i] = posx[i-1] + dt*velocidad(velx[i-1]);
@@ -63,12 +59,10 @@ float leapfrog(float x0,float y0,float vx0,float vy0,float dt,int num){
     
     float posx[puntos];
     float velx[puntos];
-    float fuerx[puntos];
-    
+        
     float posy[puntos];
     float vely[puntos];
-    float fuery[puntos];
-    
+        
     
     t[0]=0.0;
     t[1]=dt;
@@ -78,8 +72,6 @@ float leapfrog(float x0,float y0,float vx0,float vy0,float dt,int num){
     vely[0]=yprima1;
     posx[1] = posx[0] + dt*velocidad(velx[0]);
     posy[1] = posy[0] + dt*velocidad(vely[0]);
-    fuerx[0]=xprima2;
-    fuery[0]=yprima2;
     velx[1] = velx[0] + dt*fuerza(posx[0],posy[0]);
     vely[1] = vely[0] + dt*fuerza(posy[0],posx[0]);
     for(int i=2; i<puntos; i++){
@@ -94,6 +86,84 @@ float leapfrog(float x0,float y0,float vx0,float vy0,float dt,int num){
     for(int i=0; i<puntos; i++){
         outfile<<t[i]<<" "<<posx[i]<<" "<<velx[i]<<" "<<posy[i]<<" "<<vely[i]<<endl;
     }
+}
+
+
+float runge(float x0,float y0,float vx0,float vy0,float dt,int num)
+{
+    //inicio
+    float xprima1=velocidad(vx0);
+    float xprima2=fuerza(x0,y0);
+    float yprima1=velocidad(vy0);
+    float yprima2=fuerza(y0,x0);
+    int puntos = 20/dt;
+    float t[puntos];
+    
+    float posx[puntos];
+    float velx[puntos];
+    float fuerx[puntos];
+        
+    float posy[puntos];
+    float vely[puntos];
+    float fuery[puntos];
+        
+    posx[0]=x0;
+    posy[0]=y0;
+    velx[0]=xprima1;
+    vely[0]=yprima1;
+    fuerx[0]=xprima2;
+    fuery[0]=yprima2;
+    t[0]=0;
+       
+    for(int i=1; i<puntos; i++){
+        
+        t[i]=t[i-1]+dt;
+        
+        //primer paso
+        float posx_1 = posx[i-1] + (dt/2)*velocidad(velx[i-1]);
+        float posy_1 = posy[i-1] + (dt/2)*velocidad(vely[i-1]);
+        float velx_1 = velx[i-1] + (dt/2)*fuerza(posx_1,posy_1);
+        float vely_1 = vely[i-1] + (dt/2)*fuerza(posy_1,posx_1); 
+        float forx_1 = fuerza(posx_1,posy_1);
+        float fory_1 = fuerza(posy_1,posx_1);
+        
+        //segundo paso
+        
+        float posx_2 = posx[i-1] + (dt/2)*velx_1;
+        float posy_2 = posy[i-1] + (dt/2)*vely_1;
+        float velx_2 = velx[i-1] + (dt/2)*fuerza(posx_2,posy_2);
+        float vely_2 = vely[i-1] + (dt/2)*fuerza(posy_2,posx_2); 
+        float forx_2 = fuerza(posx_2,posy_2);
+        float fory_2 = fuerza(posy_2,posx_2);
+        
+        //tercer paso
+        
+        float posx_3 = posx[i-1] + (dt)*velx_2;
+        float posy_3 = posy[i-1] + (dt)*vely_2;
+        float velx_3 = velx[i-1] + (dt)*fuerza(posx_3,posy_3);
+        float vely_3 = vely[i-1] + (dt)*fuerza(posy_3,posx_3); 
+        float forx_3 = fuerza(posx_3,posy_3);
+        float fory_3 = fuerza(posy_3,posx_3);
+        
+        //promedio
+        float promediovelx = (1.0/6.0)*(velx[i-1] + 2.0*velx_1 + 2.0*velx_2 + velx_3);
+        float promediovely = (1.0/6.0)*(vely[i-1] + 2.0*vely_1 + 2.0*vely_2 + vely_3);   
+        float promediofuerx = (1.0/6.0)*(fuerx[i-1] + 2.0*forx_1 + 2.0*forx_2 + forx_3);
+        float promediofuery = (1.0/6.0)*(fuery[i-1] + 2.0*fory_1 + 2.0*fory_2 + fory_3);
+            
+            
+        posx[i]=posx[i-1]+dt*promediovelx;
+        posy[i]=posy[i-1]+dt*promediovely;
+        velx[i]=velx[i-1]+dt*promediofuerx;
+        vely[i]=vely[i-1]+dt*promediofuery;
+        
+    }
+    ofstream outfile;
+    outfile.open("datosrunge"+to_string(num)+".txt");
+    for(int i=0; i<puntos; i++){
+        outfile<<t[i]<<" "<<posx[i]<<" "<<velx[i]<<" "<<posy[i]<<" "<<vely[i]<<endl;
+    }
+       
 }
 
 
@@ -114,73 +184,14 @@ int main()
     leapfrog(x0,y0,vx0,vy0,dt1,1);
     leapfrog(x0,y0,vx0,vy0,dt2,2);
     leapfrog(x0,y0,vx0,vy0,dt3,3);
+    runge(x0,y0,vx0,vy0,dt3,1);
+    runge(x0,y0,vx0,vy0,dt3,2);
+    runge(x0,y0,vx0,vy0,dt3,3);
     
 }
 
 
-/*
-float runge(float x0,float y0,float vx0,float vy0,float dt)
-{
-    //inicio
-    float xprima1=velocidad(vx0);
-    float xprima2=fuerza(x0,y0);
-    float yprima1=velocidad(vy0);
-    float yprima2=fuerza(y0,x0);
-    float t[puntos];
-       
-    //primer paso
-    float x1 = x0 + dt/2;
-    float vx1 = vx0 + 
-*/   
-       
-         /*
-    k_1_prime1 = func_prime_1(x_old,y1_old, y2_old)
-    k_1_prime2 = func_prime_2(x_old,y1_old, y2_old)
-    
-    #first step
-    x1 = x_old+ (h/2.0)
-    y1_1 = y1_old + (h/2.0) * k_1_prime1
-    y2_1 = y2_old + (h/2.0) * k_1_prime2
-    k_2_prime1 = func_prime_1(x1, y1_1, y2_1)
-    k_2_prime2 = func_prime_2(x1, y1_1, y2_1)
-    
-    #second step
-    x2 = x_old + (h/2.0)
-    y1_2 = y1_old + (h/2.0) * k_2_prime1
-    y2_2 = y2_old + (h/2.0) * k_2_prime2
-    k_3_prime1 = func_prime_1(x2, y1_2, y2_2)
-    k_3_prime2 = func_prime_2(x2, y1_2, y2_2)
-    
-    
-    #third
-    x3 = x_old + h
-    y1_3 = y1_old + h * k_3_prime1
-    y2_3 = y2_old + h * k_3_prime2
-    k_4_prime1 = func_prime_1(x3, y1_3, y2_3)
-    k_4_prime2 = func_prime_2(x3, y1_3, y2_3)
-    
-    #fourth step
-    average_k_1 = (1.0/6.0)*(k_1_prime1 + 2.0*k_2_prime1 + 2.0*k_3_prime1 + k_4_prime1)
-    average_k_2 = (1.0/6.0)*(k_1_prime2 + 2.0*k_2_prime2 + 2.0*k_3_prime2 + k_4_prime2)
-    
-    x_new = x_old + h
-    y_1_new = y1_old + h * average_k_1
-    y_2_new= y2_old + h * average_k_2
-    return x_new, y_1_new, y_2_new
-    
-    
-    
-    
-    
-    
-    x[0]   = min_x
-y_1[0] = 1.0
-y_2[0] = 0.0    
 
-for i in range(1,n_points):
-    x[i],y_1[i],y_2[i] = RungeKuttaFourthOrderStep(x[i-1], y_1[i-1], y_2[i-1])
-*/
-        
         
         
         
--- tarea2/Plots_hw2.py
+++ tarea2/Plots_hw2.py
@@ -3,38 +3,108 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#datos
 datoseuler1=np.genfromtxt("datoseuler1.txt")
 datoseuler2=np.genfromtxt("datoseuler2.txt")
 datoseuler3=np.genfromtxt("datoseuler3.txt")
 datosleap1=np.genfromtxt("datosleap1.txt")
 datosleap2=np.genfromtxt("datosleap2.txt")
 datosleap3=np.genfromtxt("datosleap3.txt")
+datosrunge1=np.genfromtxt("datosrunge1.txt")
+datosrunge2=np.genfromtxt("datosrunge2.txt")
+datosrunge3=np.genfromtxt("datosrunge3.txt")
 datosorbit=np.genfromtxt("EarthOrbit.dat")
 
-
+#grafica1
 plt.figure(figsize=(10,20))
-plt.subplot(4,3,(1,3))
+plt.subplot(5,3,(1,3))
 plt.plot(datosorbit[:,1],datosorbit[:,2])
 plt.title("orbita datos earth orbit")
-plt.subplot(4,3,4)
+plt.subplot(5,3,4)
 plt.plot(datoseuler1[:,1],datoseuler1[:,3])
 plt.title("euler con dt=0.1")
-plt.subplot(4,3,5)
+plt.subplot(5,3,5)
 plt.plot(datoseuler2[:,1],datoseuler2[:,3])
 plt.title("euler con dt=0.01")
-plt.subplot(4,3,6)
+plt.subplot(5,3,6)
 plt.plot(datoseuler3[:,1],datoseuler3[:,3])
 plt.title("euler con dt=0.001")
-plt.subplot(4,3,7)
+plt.subplot(5,3,7)
 plt.plot(datosleap1[:,1],datosleap1[:,3])
 plt.title("leapfrog con dt=0.1")
-plt.subplot(4,3,8)
+plt.subplot(5,3,8)
 plt.plot(datosleap2[:,1],datosleap2[:,3])
 plt.title("leapfrog con dt=0.01")
-plt.subplot(4,3,9)
+plt.subplot(5,3,9)
 plt.plot(datosleap3[:,1],datosleap3[:,3])
 plt.title("leapfrog con dt=0.001")
+plt.subplot(5,3,10)
+plt.plot(datosrunge1[:,1],datosrunge1[:,3])
+plt.title("runge kutta 4orden con dt=0.1")
+plt.subplot(5,3,11)
+plt.plot(datosrunge2[:,1],datosrunge2[:,3])
+plt.title("runge kutta 4orden con dt=0.01")
+plt.subplot(5,3,12)
+plt.plot(datosrunge3[:,1],datosrunge3[:,3])
+plt.title("runge kutta 4orden con dt=0.001")
 plt.savefig("hola.png")
+
+#energias
+def energia(x,y,vx,vy):
+    masa=3.00273e-6
+    G=6.674*1e-11*(1/((1.496*1e11)**3))*1.989*1e30*(31536000**2)
+    tam=len(x)
+    ener=[]
+    for i in range(tam):
+        calculo=-G*masa*((x[i]**2+y[i]**2)**0.5)-(1/2)*masa*vx[i]**2-(1/2)*masa*vy[i]**2
+        ener.append(calculo)
+    return ener
+
+                   
+#moment angular
+def momang(x,y,vx,vy):
+    masa=3.00273e-6
+    tam=len(x)
+    mom=[]
+    for i in range(tam):
+        mom.append((masa*vx[i]+masa*vy[i])*((x[i]**2+y[i]**2)**0.5))
+    return mom
+
+#grafica2
+plt.figure(figsize=(10,20))
+plt.subplot(3,3,1)
+plt.plot(datoseuler1[:,0],energia(datoseuler1[:,1],datoseuler1[:,3],datoseuler1[:,2],datoseuler1[:,4]))
+plt.title("energia euler con dt=0.1")
+plt.subplot(3,3,2)
+plt.plot(datoseuler2[:,0],energia(datoseuler2[:,1],datoseuler2[:,3],datoseuler2[:,2],datoseuler2[:,4]))
+plt.title("energia euler con dt=0.01")
+plt.subplot(3,3,3)
+plt.plot(datoseuler3[:,0],energia(datoseuler3[:,1],datoseuler3[:,3],datoseuler3[:,2],datoseuler3[:,4]))
+plt.title("energia euler con dt=0.001")
+plt.subplot(3,3,4)
+plt.plot(datosleap1[:,0],energia(datosleap1[:,1],datosleap1[:,3],datosleap1[:,2],datosleap1[:,4]))
+plt.title("energia leapfrog con dt=0.1")
+plt.subplot(3,3,5)
+plt.plot(datosleap2[:,0],energia(datosleap2[:,1],datosleap2[:,3],datosleap2[:,2],datosleap2[:,4]))
+plt.title("energia leapfrog con dt=0.01")
+plt.subplot(3,3,6)
+plt.plot(datosleap3[:,0],energia(datosleap3[:,1],datosleap3[:,3],datosleap3[:,2],datosleap3[:,4]))
+plt.title("energia leapfrog con dt=0.001")
+plt.subplot(3,3,7)
+plt.plot(datosrunge1[:,0],energia(datosrunge1[:,1],datosrunge1[:,3],datosrunge1[:,2],datosrunge1[:,4]))
+plt.title("energia leapfrog con dt=0.1")
+plt.subplot(3,3,8)
+plt.plot(datosrunge2[:,0],energia(datosrunge2[:,1],datosrunge2[:,3],datosrunge2[:,2],datosrunge2[:,4]))
+plt.title("energia leapfrog con dt=0.01")
+plt.subplot(3,3,9)
+plt.plot(datosrunge3[:,0],energia(datosrunge3[:,1],datosrunge3[:,3],datosrunge3[:,2],datosrunge3[:,4]))
+plt.title("energia leapfrog con dt=0.001")
+plt.savefig("hola2.png")
+                   
+       
+
+
+
 
 
 
