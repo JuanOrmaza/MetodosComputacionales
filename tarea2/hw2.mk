aa6d525a7c4e5c028cf3789f3f6b44d0a9cbf419
JuanOrmaza <juanalerey@hotmail.com>
2019-07-19 12:44:03 +0000

    se implemento la funcionalidad de hw2.mk

--- tarea2/hw2.mk
+++ tarea2/hw2.mk
@@ -1,1 +1,19 @@
-hola
+all: a.out Fourier.py ODEs.cpp Plots_hw2.py datosrunge3.txt clean1 clean2
+
+Fourier.py: cara_02_grisesMF.png cara_03_grisesMF.png
+	python Fourier.py
+
+a.out: ODEs.cpp
+	g++ ODEs.cpp
+    
+datosrunge3.txt: a.out
+	./a.out
+
+Plots_hw2.py: datosrunge3.txt
+	python Plots_hw2.py
+
+clean1 :
+	rm -f *.txt
+
+clean2 :
+	rm a.out
